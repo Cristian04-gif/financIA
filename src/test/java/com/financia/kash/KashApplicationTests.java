a8f7ff6c18934aeea5d0e8584c0bb064a00f7e42
Cristian04-gif <cadrianocondor34@gmail.com>
2026-08-15 17:16:02 -0500

    primer commit

---
+++ src/test/java/com/financia/kash/KashApplicationTests.java
@@ -0,0 +1,13 @@
+package com.financia.kash;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KashApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
