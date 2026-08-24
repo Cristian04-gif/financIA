2f36e2edf96e418c6737c17c0d9f7a95d53a5ef2
JOL25 <jeankarloorozco141@gmail.com>
2026-08-23 20:14:35 -0500

    Cambios a ManageCategoriesUseCase y CategoryService con uso de los metodos del dominio

--- src/main/java/com/financia/kash/cuenta/application/port/input/ManageAccountsUseCase.java
+++ src/main/java/com/financia/kash/cuenta/application/port/input/ManageAccountsUseCase.java
@@ -1,16 +1,30 @@
 package com.financia.kash.cuenta.application.port.input;
 
+import java.math.BigDecimal;
 import java.util.List;
 import java.util.UUID;
 
 import com.financia.kash.cuenta.domain.model.Account;
+import com.financia.kash.cuenta.domain.model.AccountType;
+import com.financia.kash.transaccion.domain.model.TransactionType;
 
 public interface ManageAccountsUseCase {
+
     Account save(Account account);
 
     Account findById(UUID id);
 
     List<Account> findByUserId(UUID userId);
 
-    void delete(UUID id);
+    Account rename(UUID id, String name);
+
+    Account changeType(UUID id, AccountType type);
+
+    Account applyTransaction(
+            UUID id,
+            BigDecimal amount,
+            TransactionType transactionType
+    );
+
+    void deactivate(UUID id);
 }
--- src/main/java/com/financia/kash/cuenta/application/service/AccountService.java
+++ src/main/java/com/financia/kash/cuenta/application/service/AccountService.java
@@ -1,11 +1,14 @@
 package com.financia.kash.cuenta.application.service;
 
+import java.math.BigDecimal;
 import java.util.List;
 import java.util.UUID;
 
 import com.financia.kash.cuenta.application.port.input.ManageAccountsUseCase;
 import com.financia.kash.cuenta.application.port.output.AccountRepositoryPort;
 import com.financia.kash.cuenta.domain.model.Account;
+import com.financia.kash.cuenta.domain.model.AccountType;
+import com.financia.kash.transaccion.domain.model.TransactionType;
 
 import lombok.RequiredArgsConstructor;
 
@@ -30,7 +33,46 @@ public class AccountService implements ManageAccountsUseCase {
     }
 
     @Override
-    public void delete(UUID id) {
-        accountRepositoryPort.delete(id);
+    public Account rename(UUID id, String name) {
+
+        Account account = accountRepositoryPort.findById(id);
+
+        account.rename(name);
+
+        return accountRepositoryPort.save(account);
+    }
+
+    @Override
+    public Account changeType(UUID id, AccountType type) {
+
+        Account account = accountRepositoryPort.findById(id);
+
+        account.changeType(type);
+
+        return accountRepositoryPort.save(account);
+    }
+
+    @Override
+    public Account applyTransaction(
+            UUID id,
+            BigDecimal amount,
+            TransactionType transactionType
+    ) {
+
+        Account account = accountRepositoryPort.findById(id);
+
+        account.applyTransaction(amount, transactionType);
+
+        return accountRepositoryPort.save(account);
+    }
+
+    @Override
+    public void deactivate(UUID id) {
+
+        Account account = accountRepositoryPort.findById(id);
+
+        account.deactivate();
+
+        accountRepositoryPort.save(account);
     }
 }
